5255c7586f9d489ed9986da32c798e5c8013d58c
Paul AMICEL <paulamicel@gmail.com>
2020-09-03 14:20:17 +0200

    can create a crossing from the front

---
+++ src/components/crossing-creation-form.unit.js
@@ -0,0 +1,7 @@
+import CrossingCreationForm from './crossing-creation-form'
+
+describe('@components/crossing-creation-form', () => {
+  it('exports a valid component', () => {
+    expect(CrossingCreationForm).toBeAComponent()
+  })
+})
