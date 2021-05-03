e9a1f6e87dd381b71bcfbd7f4a6512794f01fc81
Pamicel <paulamicel@gmail.com>
2021-05-03 16:09:41 +0200

    create question store

--- src/state/modules/sqda.js
+++ src/state/modules/sqda.js
@@ -7,6 +7,7 @@ const apiUrl = process.env.API_BASE_URL
 export const state = {
   questions: [],
   fetchingQuestions: false,
+  creatingQuestion: false,
 }
 
 export const getters = {}
@@ -18,13 +19,19 @@ export const mutations = {
   END_FETCHING_QUESTIONS(state) {
     state.fetchingQuestions = false
   },
+  START_CREATING_QUESTION(state) {
+    state.creatingQuestion = true
+  },
+  END_CREATING_QUESTION(state) {
+    state.creatingQuestion = false
+  },
   REPLACE_QUESTION_LIST(state, questions) {
     state.questions = questions
   },
 }
 
 export const actions = {
-  async fetchQuestions({ commit, rootGetter }) {
+  async fetchQuestions({ commit }) {
     commit('START_FETCHING_QUESTIONS')
     try {
       const response = await axios.get(`${apiUrl}/sqda-questions`)
@@ -35,6 +42,44 @@ export const actions = {
     } catch (error) {
       commit('END_FETCHING_QUESTIONS')
       console.error(error)
+      return null
+    }
+  },
+  async createQuestion({ commit, rootGetters, dispatch }, { question }) {
+    // check logged in
+    if (!rootGetters['auth/loggedIn']) {
+      return null
+    }
+    if (question === '') {
+      return null
+    }
+    try {
+      commit('START_CREATING_QUESTION')
+      // send question
+      await axios.post(`${apiUrl}/sqda-questions`, { question })
+      commit('END_CREATING_QUESTION')
+      // reset question list
+      await dispatch('fetchQuestions')
+      return true
+    } catch (error) {
+      // console.error(error)
+      commit('END_CREATING_QUESTION')
+      // handle conflict
+      if (
+        error &&
+        error.response &&
+        (error.response.statusText === 'Conflict' ||
+          error.response.status === 409)
+      ) {
+        return {
+          error: {
+            statusText: 'Conflict',
+            status: 409,
+          },
+        }
+      }
+
+      // handle bad request
       return null
     }
   },
--- src/state/modules/sqda.unit.js
+++ src/state/modules/sqda.unit.js
@@ -28,28 +28,75 @@ describe('@state/modules/sqda', () => {
     expect(store.state.sqda.fetchingQuestions).toBe(false)
   })
 
-  it('mutation sqda/REPLACE_QUESTION_LIST to set questions to the given value', () => {
+  it('mutation sqda/REPLACE_QUESTION_LIST sets questions to the given value', () => {
     const newQuestionList = ['a', 'b', 'c']
     expect(store.state.sqda.questions).not.toEqual(newQuestionList)
     store.commit('sqda/REPLACE_QUESTION_LIST', newQuestionList)
     expect(store.state.sqda.questions).toEqual(newQuestionList)
   })
 
+  it('mutation sqda/START_CREATING_QUESTION sets creatingQuestion to true', () => {
+    expect(store.state.sqda.creatingQuestion).toBe(false)
+    store.commit('sqda/START_CREATING_QUESTION')
+    expect(store.state.sqda.creatingQuestion).toBe(true)
+  })
+
+  it('mutation sqda/END_CREATING_QUESTION sets creatingQuestion to false', () => {
+    store.commit('sqda/START_CREATING_QUESTION')
+    expect(store.state.sqda.creatingQuestion).toBe(true)
+    store.commit('sqda/END_CREATING_QUESTION')
+    expect(store.state.sqda.creatingQuestion).toBe(false)
+  })
+
   it('action sqda/fetchQuestions resolves to the list of questions', async () => {
     const questions = await store.dispatch('sqda/fetchQuestions')
     expect(questions instanceof Array).toBe(true)
   })
+
+  it('action sqda/createQuestion resolves to null with empty question', async () => {
+    await logInAsDirector(store)
+    const question = ''
+    const questionsBefore = [...store.state.sqda.questions]
+    expect(await store.dispatch('sqda/createQuestion', { question })).toBeNull()
+    expect(await store.state.sqda.questions).toEqual(questionsBefore)
+  })
+
+  const questionA = 'questionA'
+  const questionB = 'questionB'
+  const questionC = 'questionC'
+  it('action sqda/createQuestion resolves to true with correct question', async () => {
+    await logInAsDirector(store)
+    const question = questionA
+    expect(await store.dispatch('sqda/createQuestion', { question })).toBe(true)
+  })
+
+  it('action sqda/createQuestion updates the question list after success', async () => {
+    await logInAsDirector(store)
+    const question = questionB
+    const questionsBefore = [...store.state.sqda.questions]
+    expect(await store.dispatch('sqda/createQuestion', { question })).toBe(true)
+    expect(await store.state.sqda.questions).not.toEqual(questionsBefore)
+  })
+
+  it('action sqda/createQuestion resolves to error description on conflict', async () => {
+    await logInAsDirector(store)
+    const question = questionC
+    expect(await store.dispatch('sqda/createQuestion', { question })).toBe(true)
+    expect(
+      (await store.dispatch('sqda/createQuestion', { question })).error.status
+    ).toEqual(409)
+  })
 })
 
-// const directorUser = {
-//   email: 'director@te.st',
-//   password: '000000000',
-// }
+const directorUser = {
+  email: 'director@te.st',
+  password: '000000000',
+}
 
-// async function logInAsDirector(store) {
-//   const { email, password } = directorUser
-//   return await store.dispatch('auth/logIn', {
-//     email,
-//     password,
-//   })
-// }
+async function logInAsDirector(store) {
+  const { email, password } = directorUser
+  return await store.dispatch('auth/logIn', {
+    email,
+    password,
+  })
+}
