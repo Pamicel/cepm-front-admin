3e86939bbf2e23bb0f92f97726721b653c141358
Pamicel <paulamicel@gmail.com>
2021-06-01 17:37:50 +0200

    show idc words in death table

--- src/router/views/deaths.vue
+++ src/router/views/deaths.vue
@@ -28,6 +28,8 @@ export default {
       fetchingCrossing: (state) => state.crossings.fetchingSingleCrossing,
       crossing: (state) => state.crossings.selectedCrossing,
       deathList: (state) => state.deaths.deathList,
+      fetchingIdcWords: (state) => state.deaths.fetchingIdcWords,
+      idcWords: (state) => state.deaths.idcWords,
     }),
   },
   async mounted() {
@@ -45,6 +47,7 @@ export default {
     this.$store.dispatch('deaths/fetchDeaths', {
       crossingId,
     })
+    this.$store.dispatch('deaths/fetchIdcWords')
   },
   methods: {
     async createNewDeath() {
@@ -64,8 +67,44 @@ export default {
         :crossing="crossing"
       />
       <BaseIcon v-else :class="$style.loadingIcon" name="fan" spin />
-      <button @click="createNewDeath">Ajouter une mort</button>
-      <div v-for="death of deathList" :key="death.id">{{ death }}</div>
+      <br />
+      <b-button icon-right="plus" type="is-dark" rounded @click="createNewDeath"
+        >Ajouter une mort</b-button
+      >
+      <br />
+      <br />
+      <b-table
+        :data="deathList"
+        detail-key="id"
+        default-sort-direction="DESC"
+        default-sort="idc"
+      >
+        <template slot-scope="props">
+          <b-table-column field="idc" label="N° DCD" sortable numeric centered>
+            <b-tag type="is-dark"
+              >DCD-{{ props.row.idc.toString().padStart(2, '0') }}</b-tag
+            >
+          </b-table-column>
+          <b-table-column field="deathForm" label="FIRM">
+            <span>
+              <span v-if="!props.row.deathForm" class="tag is-small is-danger"
+                >Pas de FIRM</span
+              >
+              <span
+                v-else-if="props.row.deathForm && props.row.deathForm.filled"
+                class="tag is-small is-success"
+                >FIRM complet</span
+              >
+              <span v-else class="tag is-small is-info">Firm associé</span>
+            </span>
+          </b-table-column>
+          <b-table-column label="Mot de passage">
+            <b-tag>{{
+              fetchingIdcWords ? '...' : idcWords[props.row.idc - 1]
+            }}</b-tag>
+          </b-table-column>
+        </template>
+      </b-table>
     </div>
   </Layout>
 </template>
--- src/state/modules/deaths.js
+++ src/state/modules/deaths.js
@@ -5,8 +5,10 @@ const apiUrl = process.env.API_BASE_URL
 
 export const state = {
   deathList: [],
+  idcWords: [],
   fetchingDeaths: false,
   creatingDeath: false,
+  fetchingIdcWords: false,
 }
 
 export const getters = {}
@@ -26,6 +28,15 @@ export const mutations = {
   },
   END_CREATING_DEATH(state) {
     state.creatingDeath = false
+  },
+  START_FETCHING_IDC_WORDS(state) {
+    state.fetchingIdcWords = true
+  },
+  END_FETCHING_IDC_WORDS(state) {
+    state.fetchingIdcWords = false
+  },
+  SAVE_IDC_WORDS(state, newList = []) {
+    state.idcWords = newList
   },
 }
 
@@ -61,6 +72,23 @@ export const actions = {
     } catch (error) {
       console.error(error)
       commit('END_CREATING_DEATH')
+      return null
+    }
+  },
+  async fetchIdcWords({ rootGetters, commit, rootState }) {
+    if (!rootGetters['auth/loggedIn']) {
+      return null
+    }
+
+    commit('START_FETCHING_IDC_WORDS')
+    try {
+      const { data } = await axios.get(`${apiUrl}/death/idc-words`)
+      commit('SAVE_IDC_WORDS', data)
+      commit('END_FETCHING_IDC_WORDS')
+      return true
+    } catch (error) {
+      console.error(error)
+      commit('END_FETCHING_IDC_WORDS')
       return null
     }
   },
